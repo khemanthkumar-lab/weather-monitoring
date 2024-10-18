29931342e119e9ad54987dba939094bdf046c106
khemanthkumar-lab <khemanthkumar45@gmail.com>
2024-10-18 19:48:37 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+API_KEY = "ac2f53385d10a86a04e49699477b4a51"
+CITIES = ['Delhi', 'Mumbai', 'Chennai', 'Bangalore', 'Kolkata', 'Hyderabad']
+ALERT_THRESHOLD_TEMP = 35  # Temperature threshold for alerts (Celsius)
